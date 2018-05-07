f49c4d30d563622d314a72870f7d50707d591847
August-Z <402279076@qq.com>
2018-05-07 15:27:54 +0800

    webpack 测试

---
+++ lib/waterfall-slot.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>waterfall-slot</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'WaterfallSlot'
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
